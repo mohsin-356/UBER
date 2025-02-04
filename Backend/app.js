47fba15746f88fa5636a514d91db8bd76434921c
mohsin-356 <mohsinsaeed356@gmail.com>
2025-02-04 15:59:28 +0500

    Add CORS middleware to Express server and update package.json

--- Backend/app.js
+++ Backend/app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
+const cors = require('cors');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors());
 app.get('/',function(req, res) {
     res.send('Hello World');
 })
