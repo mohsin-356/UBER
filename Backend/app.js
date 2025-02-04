78cbb098f88ff764152fe5ab3d9f37c66db181c5
mohsin-356 <mohsinsaeed356@gmail.com>
2025-02-04 15:37:35 +0500

    Refactor app.js to export the app module instead of starting the server

--- Backend/app.js
+++ Backend/app.js
@@ -11,6 +11,4 @@ app.get('/',function(req, res) {
     res.send('Hello World');
 })
 
-app.listen(PORT, function() {
-    console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
