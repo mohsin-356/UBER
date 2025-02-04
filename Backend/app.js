55ccac8a5eedcef24ca6d9378ebd0700c277cba9
mohsin-356 <mohsinsaeed356@gmail.com>
2025-02-04 15:41:05 +0500

    Refactor server setup by creating server.js and updating main entry in package.json

--- Backend/app.js
+++ Backend/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
-const PORT=process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
