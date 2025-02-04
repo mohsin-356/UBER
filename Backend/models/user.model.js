c905e7a474ca50f4af537c6d0d5bb50ad0e943bc
mohsin-356 <mohsinsaeed356@gmail.com>
2025-02-04 16:41:15 +0500

    Add user model with authentication methods and update dependencies

---
+++ Backend/models/user.model.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+//here we defien our user schema for uber driver
+const userSchema = new mongoose.Schema({
+  fullname: {
+    firstname: {
+      type: String,
+      required: true,
+      minlength: [3, "Too short, min is 3 characters"],
+    },
+    lastname: {
+      type: String,
+      required: true,
+      minlength: [3, "Too short, min is 3 characters"],
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    minlength: [5, "Too short, min is 6 characters"]
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  socketId: {
+    type: String,
+  }
+});
+//creating some methods for the user schema
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+    return token;
+};
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+userSchema.statics.hashPassword = async function (password) {
+    return await bcrypt.hash(password, 10);
+}
+
+//exporting the user schema
+module.exports = mongoose.model("user", userSchema);
