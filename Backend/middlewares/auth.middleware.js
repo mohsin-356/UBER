f682a313d4c126efba68e6bc953ee21dee2e1a6f
mohsin-356 <mohsinsaeed356@gmail.com>
2025-02-05 13:53:34 +0500

    Added new changes

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -1,32 +1,25 @@
-const userModel = require('../models/user.model');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const userModel = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-module.exports.authUser = async (req, res, next) => 
-{
-    const token=req.cookies.token || req.headers.authorization?.split(' ')[1];
-    if(!token)
-    {
-        return res.status(401).json({message:'Unauthorized'});
+module.exports.authUser = async (req, res, next) => {
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const isBlackListed = await blackListTokenModel.findOne({ token });
+  if (isBlackListed) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await userModel.findById(decoded._id);
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorized" });
     }
-    const isBlackListed=await blackListTokenModel.findOne({token});
-    if(isBlackListed)
-    {
-        return res.status(401).json({message:'Unauthorized'});
-    }
-    try
-    {
-        const decoded=jwt.verify(token,process.env.JWT_SECRET);
-        const user=await userModel.findById(decoded._id);
-        if(!user)
-        {
-            return res.status(401).json({message:'Unauthorized'});
-        }
-        req.user=user;//this is the user object that we will use in the next middleware or route handler to get the user details like email, name, etc.
-        return next();
-    }
-    catch (error)
-    {
-        return res.status(401).json({message:error.message || 'Unauthorized'});
-    }
-}
+    req.user = user; //this is the user object that we will use in the next middleware or route handler to get the user details like email, name, etc.
+    return next();
+  } catch (error) {
+    return res.status(401).json({ message: error.message || "Unauthorized" });
+  }
+};
