1d9f9e94873aadd17f1c709c1d5919337021a41a
mohsin-356 <mohsinsaeed356@gmail.com>
2025-02-15 11:05:56 +0500

    Add routing and new pages for user and captain authentication

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <div className='h-screen pt-8 flex justify-between flex-col w-full bg-red-400'>
+        <img className='w-16 ml-8' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+        <div className='bg-white pb-7 py-4 px-4'>
+          <h2 className='text-3xl font-bold'>Get started with Uber</h2>
+          <button className='w-full bg-black text-white py-3 rounded mt-5'>Continue</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
