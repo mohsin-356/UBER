efcf505e9a9507e72ac1ad61fec8957e877ae923
mohsin-356 <mohsinsaeed356@gmail.com>
2025-02-15 12:01:44 +0500

    Add routing and new pages for user and captain authentication

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div>
-      <div className='h-screen pt-8 flex justify-between flex-col w-full bg-red-400'>
-        <img className='w-16 ml-8' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+      <div className='bg-cover bg-center bg-[url(https://images.unsplash.com/photo-1593950315186-76a92975b60c?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] h-screen pt-8 flex justify-between flex-col w-full bg-red-400'>
+        <img className='w-18 ml-8' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
         <div className='bg-white pb-7 py-4 px-4'>
           <h2 className='text-3xl font-bold'>Get started with Uber</h2>
-          <button className='w-full bg-black text-white py-3 rounded mt-5'>Continue</button>
+          <Link to={'/login'} className='flex items-center justify-center w-full bg-black text-white py-3 rounded mt-5'>Continue</Link>
         </div>
       </div>
     </div>
