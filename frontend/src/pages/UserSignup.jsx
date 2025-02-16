2d9a5a29ec644d49718270eed8fd495c28878e9a
mohsin-356 <mohsinsaeed356@gmail.com>
2025-02-16 12:27:04 +0500

    Add axios and dotenv dependencies; update UserSignup component for navigation

--- frontend/src/pages/UserSignup.jsx
+++ frontend/src/pages/UserSignup.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+
 const UserSignup = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
