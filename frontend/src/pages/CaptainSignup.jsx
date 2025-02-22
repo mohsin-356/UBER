9c7871de22a72f6af9bd0cf3e7b5e092ca5190f7
mohsin-356 <mohsinsaeed356@gmail.com>
2025-02-22 09:51:58 +0500

    Refactor UserProtectWrapper to use useEffect for token validation and improve navigation logic

--- frontend/src/pages/CaptainSignup.jsx
+++ frontend/src/pages/CaptainSignup.jsx
@@ -1,11 +1,19 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Link } from 'react-router-dom';
+import {CaptainDataContext} from '../context/CaptainContext';
 const CaptainSignup = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [firstName, setFirstname] = useState('');
     const [lastName, setLastName] = useState('');
-    const [captainData, setCaptainData] = useState({});
+    // const [captainData, setCaptainData] = useState({});
+
+    const [vehicleColor,setVehicleColor]=useState('');
+    const [vehiclePlate,setVehiclePlate]=useState('');
+    const [vehicleCapacity,setVehicleCapacity]=useState('');
+    const [vehicleType,setVehicleType]=useState('');
+
+    const {captain, setCaptain} = useContext(CaptainDataContext);
     const submitHandler = (e) => {
       e.preventDefault();
       setCaptainData({
@@ -58,6 +66,7 @@ const CaptainSignup = () => {
           required
           type="email"
           name="email"
+          placeholder='email address'
         />
         <h3 className="text-base font-medium mb-2">What's your Password</h3>
         <input
@@ -67,7 +76,59 @@ const CaptainSignup = () => {
           required
           type="password"
           name="password"
+          placeholder='enter your password'
         />
+        <h3 className='text-lg font-medium mb-2'>Vehicle Information</h3>
+        <div className='flex gap-4 mb-7'>
+            <input
+              required
+              className='bg-[#eeeeee] w-1/2 rounded-lg px-4 py-2 border text-lg placeholder:text-base'
+              type="text"
+              placeholder='Vehicle Color'
+              value={vehicleColor}
+              onChange={(e) => {
+                setVehicleColor(e.target.value)
+              }}
+            />
+            <input
+              required
+              className='bg-[#eeeeee] w-1/2 rounded-lg px-4 py-2 border text-lg placeholder:text-base'
+              type="text"
+              placeholder='Vehicle Plate'
+              value={vehiclePlate}
+              onChange={(e) => {
+                setVehiclePlate(e.target.value)
+              }}
+            />
+          </div>
+
+          <div className='flex gap-4 mb-7'>
+            <input
+              required
+              className='bg-[#eeeeee] w-1/2 rounded-lg px-4 py-2 border text-lg placeholder:text-base'
+              type="number"
+              placeholder='Vehicle Capacity'
+              value={vehicleCapacity}
+              onChange={(e) => {
+                setVehicleCapacity(e.target.value)
+              }}
+            />
+            <select
+              required
+              className='bg-[#eeeeee] w-1/2 rounded-lg px-4 py-2 border text-lg placeholder:text-base'
+              value={vehicleType}
+              onChange={(e) => {
+                setVehicleType(e.target.value)
+              }}
+            >
+              <option value="" disabled>Select Vehicle Type</option>
+              <option value="car">Car</option>
+              <option value="auto">Auto</option>
+              <option value="moto">Moto</option>
+            </select>
+          </div>
+
+
         <button className='bg-[#111] text-white font-semibold mb-5 rounded px-4 py-2 border w-full text-base placeholder:text-base'>
           Sign up
         </button>
