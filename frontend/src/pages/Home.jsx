859b87bbbf62a00f7b6bc71f0e877e69812fc804
mohsin-356 <mohsinsaeed356@gmail.com>
2025-03-13 21:42:29 +0500

    Add GSAP and Remixicon dependencies; enhance Home component with animation and state management

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,36 +1,81 @@
-import React, { useState } from 'react'
-
+import React, { useRef, useState } from "react";
+import { useGSAP } from '@gsap/react'
+import gsap from "gsap";
 const Home = () => {
-  const [pickup, setPickup] = useState('');
-  const [destination, setDestination] = useState('');
-  const submitHandler=(e)=>{
+  const [pickup, setPickup] = useState("");
+  const [destination, setDestination] = useState("");
+  const [panelOpen, setPanelOpen] = useState(false);
+  const panelRef = useRef(null);
+  const submitHandler = (e) => {
     e.preventDefault();
-  }
+  };
+  useGSAP(function () {
+    if (panelOpen) {
+      gsap.to(panelRef.current, {
+        height: '70%', duration:
+          0.5
+      })
+    }
+    else {
+      gsap.to(panelRef.current, {
+        height: '0%', duration:
+          0.5
+      })
+    }
+  }, [panelOpen]);
   return (
-    <div className='h-screen relative overflow-hidden'>
-      <img className='w-16 absolute left-5 top-5' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
-      <div className='h-screen w-screen'>
+    <div className="h-screen relative overflow-hidden">
+      <img
+        className="w-16 absolute left-5 top-5"
+        src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
+        alt=""
+      />
+      <div className="h-screen w-screen">
         {/* placeholder image */}
-        <img className='h-full w-full object-cover' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="" />
+        <img
+          className="h-full w-full object-cover"
+          src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif"
+          alt=""
+        />
       </div>
-      <div className=' flex flex-col justify-end h-screen absolute top-0 w-full'>
-        <div className='h-[30%] p-6 bg-white relative'>
-          <h4 className='text-2xl font-semibold'>Find a trip</h4>
-          <form className='relative py-3' onSubmit={(e) => {
-            submitHandler(e)
-          }}>
+      <div className=" flex flex-col justify-end h-screen absolute top-0 w-full">
+        <div className="h-[30%] p-6 bg-white relative">
+          <h4 className="text-2xl font-semibold">Find a trip</h4>
+          <form
+            className="relative py-3"
+            onSubmit={(e) => {
+              submitHandler(e);
+            }}
+          >
             <div className="line absolute h-16 w-1 top-[50%] -translate-y-1/2 left-5 bg-gray-950 rounded-full"></div>
-            <input className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full' type="text" placeholder='Add a pick-up location' />
-            <input className='bg-[#eee] px-12 py-2 text-lg rounded-lg w-full  mt-3' type="text" placeholder='Enter your destination' />
+            <input
+              className="bg-[#eee] px-12 py-2 text-lg rounded-lg w-full"
+              type="text"
+              placeholder="Add a pick-up location"
+              value={pickup}
+              onChange={(e) => setPickup(e.target.value)}
+              onClick={() => {
+                setPanelOpen(true)
+              }}
+            />
+            <input
+              className="bg-[#eee] px-12 py-2 text-lg rounded-lg w-full  mt-3"
+              type="text"
+              placeholder="Enter your destination"
+              value={destination}
+              onChange={(e) => setDestination(e.target.value)}
+              onClick={() => {
+                setPanelOpen(true)
+              }}
+            />
           </form>
-
         </div>
-        <div className='bg-red-500 h-0'>
+        <div ref={panelRef} className="bg-red-500 h-0">
 
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
