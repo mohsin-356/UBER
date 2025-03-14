bbad485e697ae202cc59954457cd34076b6859a5
mohsin-356 <mohsinsaeed356@gmail.com>
2025-03-14 14:13:46 +0500

    Enhance Home component with additional panel close functionality and animations

--- frontend/src/components/LocationSearchPanel.jsx
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 const LocationSearchPanel = () => {
-  return (
-    <div>LocationSearchPanel</div>
-  )
+    return (
+        <div>
+            {/* THIS IS A SAMPLE DATA */}
+            <div className="flex gap-4 my-2 items-center justify-items-start border-b-2 border-gray-200 pb-2">
+               <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12  rounded-full'> <i class="ri-map-pin-line text-2xl text-gray-500"></i></h2>
+                <h4 className="text-lg font-semibold">24B, Near Quetta's Cafe textiles coding school, Faisalabad</h4>
+            </div>
+        </div>
+    )
 }
 
-export default LocationSearchPanel
+export default LocationSearchPanel;
