631c20e1a7fc7cc613ec7b93db1f9c636877c3cc
mohsin-356 <mohsinsaeed356@gmail.com>
2025-03-14 15:07:03 +0500

    Enhance Home component with additional panel close functionality and animations

--- frontend/src/components/LocationSearchPanel.jsx
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -4,9 +4,16 @@ const LocationSearchPanel = () => {
     return (
         <div>
             {/* THIS IS A SAMPLE DATA */}
-            <div className="flex gap-4 my-2 items-center justify-items-start border-b-2 border-gray-200 pb-2">
-               <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12  rounded-full'> <i class="ri-map-pin-line text-2xl text-gray-500"></i></h2>
-                <h4 className="text-lg font-semibold">24B, Near Quetta's Cafe textiles coding school, Faisalabad</h4>
+            <div className="flex gap-4 my-2 items-center justify-items-start border-2 border-gray-300 rounded-lg p-2">
+                <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12  rounded-full'> <i class="ri-map-pin-line text-2xl text-gray-500"></i></h2>
+                <h4 className="text-md font-semibold">24B, Near Quetta's Cafe textiles coding school, Faisalabad</h4>
+            </div>
+            <div className="flex gap-4 my-2 items-center justify-items-start border-2 border-gray-300 rounded-lg p-2">
+                <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12  rounded-full'> <i class="ri-map-pin-line text-2xl text-gray-500"></i></h2>
+                <h4 className="text-md font-semibold">24B, Near Quetta's Cafe textiles coding school, Faisalabad</h4>
+            </div>
+            <div className="fixed z-10 buttom-0">
+                <div><img src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1652995234/assets/92/8d4288-e896-4333-9bc2-c60c49f2a095/original/UberXL_Black_v2.png" alt="car" /></div>
             </div>
         </div>
     )
