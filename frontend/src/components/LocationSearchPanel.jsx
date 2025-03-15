847a817806a675257778bc3bd9d1a3eafd78549c
mohsin-356 <mohsinsaeed356@gmail.com>
2025-03-15 14:38:58 +0500

    Refactor LocationSearchPanel styling and update Home component layout for improved design

--- frontend/src/components/LocationSearchPanel.jsx
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -4,11 +4,11 @@ const LocationSearchPanel = () => {
     return (
         <div>
             {/* THIS IS A SAMPLE DATA */}
-            <div className="flex gap-4 my-2 items-center justify-items-start border-2 border-gray-300 rounded-lg p-2">
+            <div className="flex gap-4 border-2 p-3 border-gray-50 active:border-black rounded-xl my-2 items-center justify-items-start">
                 <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12  rounded-full'> <i className="ri-map-pin-line text-2xl text-gray-500"></i></h2>
                 <h4 className="text-md font-semibold">24B, Near Quetta's Cafe textiles coding school, Faisalabad</h4>
             </div>
-            <div className="flex gap-4 my-2 items-center justify-items-start border-2 border-gray-300 rounded-lg p-2">
+            <div className="flex gap-4 border-2 p-3 border-gray-50 active:border-black rounded-xl my-2 items-center justify-items-start">
                 <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12  rounded-full'> <i className="ri-map-pin-line text-2xl text-gray-500"></i></h2>
                 <h4 className="text-md font-semibold">24B, Near Quetta's Cafe textiles coding school, Faisalabad</h4>
             </div>
