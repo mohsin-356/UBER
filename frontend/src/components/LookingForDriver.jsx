4f76bed01ca83228eeb01a53b9b638568484d0ee
mohsin-356 <mohsinsaeed356@gmail.com>
2025-03-27 22:14:11 +0500

    Update ConfirmRide and LookingForDriver components to accept new props

--- frontend/src/components/LookingForDriver.jsx
+++ frontend/src/components/LookingForDriver.jsx
@@ -4,7 +4,7 @@ const LookingForDriver = (props) => {
     return (
         <div>
             <h5 onClick={() => {
-                props.setVehiclePanel(false);
+                props.setVehicleFound(false);
             }} className="p-1 text-center w-[93%] absolute top-0"><i className="text-3xl text-gray-500 ri-arrow-down-wide-line"></i></h5>
             <h3 className="text-2xl font-semibold mb-5">Looking for Captain</h3>
 
