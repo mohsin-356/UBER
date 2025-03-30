672df9836f56c1e53963dc7a44440043438ce3b2
mohsin-356 <mohsinsaeed356@gmail.com>
2025-03-30 14:23:05 +0500

    Update WaitingForDriver component and Home page to manage waiting state

--- frontend/src/pages/Riding.jsx
+++ frontend/src/pages/Riding.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Riding = () => {
   return (
-    <div>Riding</div>
+    <div className='h-screen'>
+        <div className='h-1/2'>
+        <img className="w-16 absolute left-5 top-5" src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="Uber Logo" />
+        </div>
+        <div className='h-1/2'>
+        <button>Make a Payment</button>
+        </div>
+    </div>
   )
 }
 
