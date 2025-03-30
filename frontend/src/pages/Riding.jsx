f667e9d2fde0730e843ed7b85b7fa1e8bff61722
mohsin-356 <mohsinsaeed356@gmail.com>
2025-03-30 14:30:19 +0500

    Refactor Riding component layout and improve structure

--- frontend/src/pages/Riding.jsx
+++ frontend/src/pages/Riding.jsx
@@ -6,7 +6,7 @@ const Riding = () => {
             <div className='h-1/2'>
                 <img className="w-16 absolute left-5 top-5" src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="Uber Logo" />
             </div>
-            <div className='h-1/2'>
+            <div className='h-1/2 p-4'>
 
                 <div className='flex items-center justify-between'>
                     <img className='h-12' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1652995234/assets/92/8d4288-e896-4333-9bc2-c60c49f2a095/original/UberXL_Black_v2.png" alt="car image" />
@@ -19,14 +19,6 @@ const Riding = () => {
 
                 <div className='flex gap-2 items-center justify-between flex-col'>
                     <div className='w-full mt-5'>
-
-                        <div className='flex items-center gap-5 p-3 border-gray-300  border-b-2'>
-                            <i class="text-lg ri-map-pin-2-fill"></i>
-                            <div>
-                                <h3 className='text-lg font-medium'>562/11-A</h3>
-                                <p className='text-sm -mt-1 text-gray-600'>Saleemi chowk,Faisalabad</p>
-                            </div>
-                        </div>
 
                         <div className='flex items-center gap-5 p-3 border-gray-300 border-b-2'>
                             <i className="ri-map-pin-3-line"></i>
@@ -46,7 +38,7 @@ const Riding = () => {
 
                     </div>
                 </div>
-                <button>Make a Payment</button>
+                <button className='w-full mt-5 bg-green-600 text-white font-semibold p-2 rounded-lg' >Make a Payment</button>
             </div>
         </div>
     )
