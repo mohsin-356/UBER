44648e88a7a5ca8fe976269e88336f866333d2e9
mohsin-356 <mohsinsaeed356@gmail.com>
2025-04-12 23:44:15 +0500

    Update WaitingForDriver component and Home page to manage waiting state

--- frontend/src/pages/CaptainHome.jsx
+++ frontend/src/pages/CaptainHome.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 const CaptainHome = () => {
     return (
         <div className='h-screen'>
-            <Link to={"/home"} className='fixed right-2 top-2 h-10 w-10 bg-white flex items-center justify-center rounded-full'>
+           <div className='fix'>
+            <img className='w-16' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+            <Link to='/home' className='fixed right-2 top-2 h-10 w-10 bg-white flex items-center justify-center rounded-full'>
                 <i className="text-lg font-medium ri-home-5-line"></i>
             </Link>
+           </div>
             <div className='h-1/2'>
                 <img
                     className="h-full w-full object-cover"
