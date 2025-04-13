bcb58272c8a525d54312fe6e11ffc39d121a0633
mohsin-356 <mohsinsaeed356@gmail.com>
2025-04-13 11:07:23 +0500

    Update CaptainHome component layout for improved responsiveness

--- frontend/src/pages/CaptainHome.jsx
+++ frontend/src/pages/CaptainHome.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 const CaptainHome = () => {
     return (
         <div className='h-screen'>
-           <div className='fixed p-3 top-0'>
+           <div className='fixed p-3 top-0 flex items-center justify-between w-screen'>
             <img className='w-16' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
-            <Link to='/home' className='fixed right-2 top-2 h-10 w-10 bg-white flex items-center justify-center rounded-full'>
+            <Link to='/home' className='h-10 w-10 bg-white flex items-center justify-center rounded-full'>
                 <i className="text-lg font-medium ri-home-5-line"></i>
             </Link>
            </div>
