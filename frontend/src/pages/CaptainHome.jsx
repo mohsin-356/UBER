a5d88165f005c5cd36d963e5abee9326579697b7
mohsin-356 <mohsinsaeed356@gmail.com>
2025-04-13 14:30:37 +0500

    Improve styling for earned amount and hours online in CaptainHome component

--- frontend/src/pages/CaptainHome.jsx
+++ frontend/src/pages/CaptainHome.jsx
@@ -25,61 +25,29 @@ const CaptainHome = () => {
                 </div>
                 <div>
                     <h4 className='text-xl font-semibold'>$295.2</h4>
-                    <p className='text-sm font-medium bg-gray-600'>Earned</p>
+                    <p className='text-sm font-medium bg-green-500 rounded-2xl pr-1 pl-2'>Earned</p>
                 </div>
                 </div>
 
                 <div className='flex justify-center gap-5 items-start'>
                     <div className='text-center'>
-                        <i className='text-3xl font-thin ri-timer-2-line'></i>
-                        <h5>10.2</h5>
+                        <i className='text-3xl mb-2 font-thin ri-timer-2-line'></i>
+                        <h5 className='text-lg font-medium'>10.2</h5>
                         <p className='text-sm text-gray-600'>Hours Online</p>
                     </div>
 
                     <div className='text-center'>
-                        <i className='text-3xl font-thin ri-speed-up-fill'></i>
-                        <h5>10.2</h5>
+                        <i className='text-3xl mb-2 font-thin ri-speed-up-fill'></i>
+                        <h5 className='text-lg font-medium'>10.2</h5>
                         <p className='text-sm text-gray-600'>Hours Online</p>
                     </div>
 
                     <div className='text-center'>
-                        <i className='text-3xl font-thin ri-booklet-line'></i>
-                        <h5>10.2</h5>
+                        <i className='text-3xl mb-2 font-thin ri-booklet-line'></i>
+                        <h5 className='text-lg font-medium'>10.2</h5>
                         <p className='text-sm text-gray-600'>Hours Online</p>
                     </div>
                 </div>
-
-                {/* <div className='flex items-center justify-between'>
-                    <img className='h-12' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1652995234/assets/92/8d4288-e896-4333-9bc2-c60c49f2a095/original/UberXL_Black_v2.png" alt="car image" />
-                    <div className='text-right'>
-                        <h2 className='text-lg font-medium'>Zeeshan Nazam</h2>
-                        <h4 className='text-xl font-semibold -mt-1 -mb-1'>MP04 AB 1234</h4>
-                        <p className='text-sm text-gray-600'>Maruti Suzuki Alto</p>
-                    </div>
-                </div>
-                <div className='flex gap-2 items-center justify-between flex-col'>
-                    <div className='w-full mt-5'>
-
-                        <div className='flex items-center gap-5 p-3 border-gray-300 border-b-2'>
-                            <i className="ri-map-pin-3-line"></i>
-                            <div>
-                                <h3 className='text-lg font-medium'>NTU-manawala</h3>
-                                <p className='text-sm -mt-1 text-gray-600'>sheikhupura road Manawala,Faisalabad</p>
-                            </div>
-                        </div>
-
-                        <div className='flex items-center gap-5 p-3'>
-                            <i className="text-lg ri-currency-line"></i>
-                            <div>
-                                <h3 className='text-lg font-medium'>$120</h3>
-                                <p className='text-sm -mt-1 text-gray-600'>Cash Cash</p>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-                <button className='w-full mt-5 bg-green-600 text-white font-semibold p-2 rounded-lg' >Make a Payment</button> */}
-
             </div>
         </div>
     )
