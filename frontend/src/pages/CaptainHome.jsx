a3b433aaa1c5091e542f56b8c8a20c3eae2455ad
mohsin-356 <mohsinsaeed356@gmail.com>
2025-04-13 14:01:50 +0500

    Improve styling and layout for user information in CaptainHome component

--- frontend/src/pages/CaptainHome.jsx
+++ frontend/src/pages/CaptainHome.jsx
@@ -18,13 +18,13 @@ const CaptainHome = () => {
             </div>
 
             <div className='h-1/2 p-4'>
-                <div>
-                <div>
-                    <img className='flex items-center justify-between' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdlMd7stpWUCmjpfRjUsQ72xSWikidbgaI1w&s" alt="" />
-                    <h4>Usman Patel</h4>
+                <div className='flex items-center justify-between'>
+                <div className='flex items-center justify-start gap-3'>
+                    <img className='h-10 w-10 rounded-full object-cover' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdlMd7stpWUCmjpfRjUsQ72xSWikidbgaI1w&s" alt="" />
+                    <h4 className='text-lg font-medium'>Usman Patel</h4>
                 </div>
                 <div>
-                    <h4>$295.2</h4>
+                    <h4 className='text-xl font-semibold'>$295.2</h4>
                     <p>Earned</p>
                 </div>
                 </div>
