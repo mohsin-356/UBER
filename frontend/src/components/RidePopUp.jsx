e0808bb3a07062e46800b826b14c3bcdddeaff8f
mohsin-356 <mohsinsaeed356@gmail.com>
2025-04-15 14:50:28 +0500

    Adjust margin for layout in CaptainHome component

--- frontend/src/components/RidePopUp.jsx
+++ frontend/src/components/RidePopUp.jsx
@@ -6,10 +6,10 @@ const RidePopUp = () => {
             <h5 onClick={() => {
                 props.setConfirmRidePanel(false);
             }} className="p-1 text-center w-[93%] absolute top-0"><i className="text-3xl text-gray-500 ri-arrow-down-wide-line"></i></h5>
-            <h3 className="text-2xl font-semibold mb-5">Confirm your Ride</h3>
+            <h3 className="text-2xl font-semibold mb-5">New Ride Available !</h3>
 
             <div className='flex gap-2 items-center justify-between flex-col'>
-                <img className='h-20' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1652995234/assets/92/8d4288-e896-4333-9bc2-c60c49f2a095/original/UberXL_Black_v2.png" alt="" />
+                {/* <img className='h-20' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1652995234/assets/92/8d4288-e896-4333-9bc2-c60c49f2a095/original/UberXL_Black_v2.png" alt="" /> */}
 
                 <div className='w-full mt-5'>
 
@@ -42,6 +42,11 @@ const RidePopUp = () => {
                 <button onClick={() => {
                     props.setVehicleFound(true);
                 }} className='w-full mt-5 bg-green-600 text-white font-semibold p-2 rounded-lg'>Confirm</button>
+
+                <button onClick={() => {
+
+                }} className='w-full mt-5 bg-gray-300 text-gray-800 font-semibold p-2 rounded-lg'>Ignore</button>
+
             </div>
         </div>
     )
