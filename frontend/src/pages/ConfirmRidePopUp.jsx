dac9865b0d1715bc938e72549108116aef5f28c3
mohsin-356 <mohsinsaeed356@gmail.com>
2025-05-06 19:20:25 +0500

    fix: correct syntax errors in ConfirmRidePopUp component

--- frontend/src/pages/ConfirmRidePopUp.jsx
+++ frontend/src/pages/ConfirmRidePopUp.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const ConfirmRidePopUp = () => {
   return (
@@ -6,4 +6,4 @@ const ConfirmRidePopUp = () => {
   )
 }
 
-export default ConfirmRidePopUp
+export default ConfirmRidePopUp;
