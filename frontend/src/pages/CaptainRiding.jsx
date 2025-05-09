846076d6c175f582bcb51136413aa9a7ae63e957
mohsin-356 <mohsinsaeed356@gmail.com>
2025-05-09 16:01:09 +0500

    customizing the component

--- frontend/src/pages/CaptainRiding.jsx
+++ frontend/src/pages/CaptainRiding.jsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 const CaptainRiding = () => {
   return (
-    <div>CaptainRiding</div>
+    <div className='h-screen'>
+            <div className='fixed p-6 top-0 flex items-center justify-between w-screen'>
+                <img className='w-16' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+                <Link to='/home' className='h-10 w-10 bg-white flex items-center justify-center rounded-full'>
+                    <i className="text-lg font-medium ri-home-5-line"></i>
+                </Link>
+            </div>
+            <div className='h-3/5'>
+                <img className="h-full w-full object-cover" src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="Background" />
+            </div>
+
+            <div className='h-2/5 p-6'>
+                <CaptainDetails />
+            </div>
+            <div ref={ridePopupPanelRef} className="fixed w-full z-10 bottom-0 bg-white translate-y-full px-3 py-10 pt-12">
+               <RidePopUp setRidePopupPanel={setRidePopupPanel} setConfirmRidePopupPanel={setConfirmRidePopupPanel} />
+            </div>
+
+            <div ref={confirmRidePopupPanelRef} className="fixed h-screen w-full z-10 bottom-0 bg-white translate-y-full px-3 py-10 pt-12">
+               <ConfirmRidePopUp setConfirmRidePopupPanel={setConfirmRidePopupPanel} setRidePopupPanel={setRidePopupPanel} />
+            </div>
+        </div>
   )
 }
 
