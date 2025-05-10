f6216060b1b610e835f404c13405cebaac2b552f
mohsin-356 <mohsinsaeed356@gmail.com>
2025-05-10 23:44:56 +0500

    customizing the component

--- frontend/src/pages/CaptainRiding.jsx
+++ frontend/src/pages/CaptainRiding.jsx
@@ -4,13 +4,16 @@ import { Link } from 'react-router-dom'
 const CaptainRiding = () => {
   return (
     <div className='h-screen'>
+        <h5 className="p-1 text-center w-[93%] absolute top-0" onClick={() => {
+           
+            }}  ><i className="text-3xl text-gray-500 ri-arrow-down-wide-line"></i></h5>
             <div className='fixed p-6 top-0 flex items-center justify-between w-screen'>
                 <img className='w-16' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
                 <Link to='/home' className='h-10 w-10 bg-white flex items-center justify-center rounded-full'>
                     <i className="text-lg font-medium ri-home-5-line"></i>
                 </Link>
             </div>
-            <div className='h-3/5'>
+            <div className='h-4/5'>
                 <img className="h-full w-full object-cover" src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="Background" />
             </div>
 
