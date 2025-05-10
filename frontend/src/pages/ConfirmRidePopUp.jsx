f12006fbc0c5d25cf9ef55acbb7c98855ba16559
mohsin-356 <mohsinsaeed356@gmail.com>
2025-05-11 00:01:29 +0500

    customizing the component

--- frontend/src/pages/ConfirmRidePopUp.jsx
+++ frontend/src/pages/ConfirmRidePopUp.jsx
@@ -46,11 +46,15 @@ const ConfirmRidePopUp = (props) => {
 
         </div>
         <div className='mt-6'>
-          <Link to='/captain-riding' className='w-full mt-5 flex justify-center bg-green-600 text-white font-semibold p-2 rounded-lg'>Confirm Ride</Link>
-
-          <button onClick={() => {
-            props.setConfirmRidePopupPanel(false);
-          }} className='w-full mt-5 bg-black text-white font-semibold p-2 rounded-lg'>Cancel</button>
+          <form onSubmit={(e) => {
+            submitHandler(e);
+          }}>
+            <input type='text' placeholder='enter OTP'/>
+            <Link to='/captain-riding' className='w-full mt-5 flex justify-center bg-green-600 text-white font-semibold p-2 rounded-lg'>Confirm Ride</Link>
+            <button onClick={() => {
+              props.setConfirmRidePopupPanel(false);
+            }} className='w-full mt-5 bg-black text-white font-semibold p-2 rounded-lg'>Cancel</button>
+          </form>
         </div>
       </div>
     </div>
