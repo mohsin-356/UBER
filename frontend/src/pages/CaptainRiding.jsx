f54ab4cc812639901f5803eb996ba80fd106ad84
mohsin-356 <mohsinsaeed356@gmail.com>
2025-05-11 13:03:17 +0500

    customizing the component

--- frontend/src/pages/CaptainRiding.jsx
+++ frontend/src/pages/CaptainRiding.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const CaptainRiding = () => {
+
+    useState(false)
 
     useGSAP(function () {
         if (confirmRidePopupPanel) {
