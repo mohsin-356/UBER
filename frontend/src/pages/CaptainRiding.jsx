5fa1911362c10ab00de85052333f3bdc5e1fbde0
mohsin-356 <mohsinsaeed356@gmail.com>
2025-05-11 15:30:13 +0500

    customizing the component

--- frontend/src/pages/CaptainRiding.jsx
+++ frontend/src/pages/CaptainRiding.jsx
@@ -40,13 +40,12 @@ const CaptainRiding = () => {
                 {/* <h5 className="p-1 text-center w-[95%] absolute top-0" onClick={() => {
                 }}  ><i className="text-3xl text-gray-500 ri-arrow-down-wide-line"></i></h5> */}
                 <h1 className='text-xl font-semibold'>4KM away</h1>
-                <button className='bg-green-600 text-white font-semibold p-3 px-10 rounded-lg'>Complete Ride</button>
+                <button onClick={() => {
+                setFinishRidePanel(true);
+            }}  className='bg-green-600 text-white font-semibold p-3 px-10 rounded-lg'>Complete Ride</button>
             </div>
 
-            <div onClick={() => {
-                setFinishRidePanel(true);
-            }}
-            ref={finishRidePanelRef} className="fixed w-full z-10 bottom-0 bg-white translate-y-full px-3 py-10 pt-12">
+            <div ref={finishRidePanelRef} className="fixed w-full z-10 bottom-0 bg-white translate-y-full px-3 py-10 pt-12">
                 <FinishRide setFinishRidePanel={setFinishRidePanel} />
             </div>
 
