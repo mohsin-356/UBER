e539bb8ca56abca18a93e2f508fdd488e5aa9623
mohsin-356 <mohsinsaeed356@gmail.com>
2025-05-14 09:28:22 +0500

    customizing the controller

--- Backend/controllers/map.controller.js
+++ Backend/controllers/map.controller.js
@@ -1,10 +1,16 @@
 const mapService = require('../services/maps.service');
+const {validationResult} = require('express-validator');
 module.exports.getCoordinates = async (req, res) => {
+    // Validate the request
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const { address } = req.query;
+    // if (!address) {
+    //     return res.status(400).json({ message: 'Address is required' });
+    // }
     try {
-        const { address } = req.query;
-        if (!address) {
-            return res.status(400).json({ message: 'Address is required' });
-        }
         const coordinates = await mapService.getAddressCoordinate(address);
         return res.status(200).json({ coordinates });
     } catch (error) {
--- Backend/routes/maps.routes.js
+++ Backend/routes/maps.routes.js
@@ -2,8 +2,11 @@ const express=require('express');
 const router=express.Router();
 const authMiddleware = require("../middlewares/auth.middleware");
 const mapController=require("../controllers/map.controller");
+const { query } = require("express-validator");
 
-router.get('/get-coordinates',authMiddleware.authUser,mapController);
+router.get('/get-coordinates',
+    query('address').isString().isLength({ min: 3 })
+    ,authMiddleware.authUser,mapController);
 
 
 module.exports =router;
