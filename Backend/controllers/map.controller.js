dd7b0c0a7b38e81b4fba3aecf2e2510b8efc808e
mohsin-356 <mohsinsaeed356@gmail.com>
2025-05-14 15:33:36 +0500

    customizing the controller

--- Backend/controllers/map.controller.js
+++ Backend/controllers/map.controller.js
@@ -18,7 +18,29 @@ module.exports.getCoordinates = async (req, res) => {
 module.exports.getDistanceAndTime = async (req, res) => {
   // Validate the request
   const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+    if (!errors.isEmpty())
+    {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+  try{
+    const { origin, destination } = req.query;
+    const distanceAndTime = await mapService.getDistanceAndTime(
+      origin,
+      destination
+    );
+    if (distanceAndTime.status === "OK") {
+      return res.status(200).json({
+        distance: distanceAndTime.distance,
+        duration: distanceAndTime.duration,
+      });
+    } else {
+      return res.status(404).json({ message: "Distance and time not found" });
+    }
+  }
+  catch(error)
+  {
+    console.error("Error fetching distance and time:", error.message);
+    return res.status(404).json({ message: "Distance and time not found catch Block" });
   }
 }
